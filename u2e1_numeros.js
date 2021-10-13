24366fc395d3bbe619723a758b8958653b311698
DAW <Daniel>
2021-10-13 20:57:00 +0200

    ejercicio numeros

---
+++ u2e1_numeros.js
@@ -0,0 +1,15 @@
+//ejercicio u2e1_numeros
+
+let a = 1357; //numero entero
+let b = 135.7; // numero decimal
+let c = 135e7; //numero cientifico
+let d = 01357; //numero octal
+let e = 0x1357; //numero hexadecimal
+
+
+
+alert("el numero entero es "  + a);
+alert("el numero decimal es "  + b);
+alert("el numero científico es "  + c);
+alert("el numero octal es " + d);
+alert("el numero hexadecimal es "  + e);
