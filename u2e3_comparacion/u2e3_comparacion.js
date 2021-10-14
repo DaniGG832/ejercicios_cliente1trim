35b6df468f365be8a3e9c5a8c86b0c0c511fa643
DAW <Daniel>
2021-10-14 21:17:56 +0200

    u2e3

---
+++ u2e3_comparacion/u2e3_comparacion.js
@@ -0,0 +1,59 @@
+/*Crea un programa en el que muestres el resultado de varias operaciones mediante alert, mostrando el texto exacto de la
+operación realizada y su resultado.*/
+
+
+
+// Muestra en un alert  con  la operacion 10 == 10.
+
+let operacion1 = (10==10);
+
+alert ("La operación 10==10 es: " + operacion1);
+
+// Muestra en un alert  con  la operacion 10 === 10.
+
+let operacion2 = (10===10);
+
+alert ("La operación 10===10 es: " + operacion2);
+
+// Muestra en un alert  con  la operacion 10 === 10.0.
+
+let operacion3 = (10 === 10.0);
+
+alert ("La operación 10 === 10.0 es: " + operacion3);
+
+// Muestra en un alert  con  la operacion “Laura” == “laura”.
+
+let operacion4 = ("Laura" == "laura");
+
+alert ('La operación "Laura" == "laura" es: ' + operacion4);
+
+// Muestra en un alert  con  la operacion “Laura” > “laura”.
+
+let operacion5 = ("Laura" > "laura");
+
+alert ("La operación “Laura” > “laura” es: "+operacion5);
+
+// Muestra en un alert  con  la operacion “Laura” < “laura”.
+
+let operacion6 = ("Laura" < "laura");
+
+alert ('La operación “Laura” < “laura” es: ' + operacion6);
+
+// Muestra en un alert  con  la operacion “Laura” < “laura”.
+
+let operacion7 = ("123" == 123);
+
+alert ('La operación “123” == 123 es: ' + operacion7);
+
+// Muestra en un alert  con  la operacion "123"=== 123.
+
+let operacion8 = ("123"=== 123);
+
+alert ('La operación "123” === 123 es: ' + operacion8);
+
+// Muestra en un alert  con  la operacion parseInt("123") === 123.
+
+let operacion9 = (parseInt("123") === 123);
+
+alert ('La operación parseInt(“123”) === 123 es: ' + operacion9);
+
